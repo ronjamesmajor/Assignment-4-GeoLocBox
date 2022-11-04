0a132981a8908684c995c8a123dea46bd9ac97fc
Shabs <85132269+doshabsa@users.noreply.github.com>
2022-11-04 14:45:15 -0600

    Added codes from sample projects, applicable to SQLite and Phidget

--- 270_GeoLocBox/270_GeoLocBox/Form1.cs
+++ 270_GeoLocBox/270_GeoLocBox/Form1.cs
@@ -1,11 +1,15 @@
 using Phidget22;
 using System.IO;
+using System.Windows.Forms;
 
 namespace _270_GeoLocBox
 {
     public partial class Form1 : Form
     {
         //DigitalInput button1 = null;
+
+        //Allows pathing to database file
+        //SQLDataLayer dl = new("Data source=D:/testDB.db");
 
         public Form1()
         {
@@ -20,5 +24,33 @@ namespace _270_GeoLocBox
             //button1.StateChange += Button1_StateChange1;
             //button1.Open(1000);
         }
+
+        //private void button1_Click(object sender, EventArgs e)
+        //{
+        //    dl.InsertRecord("Test", DateTime.Now);
+        //}
+
+        //private void btnLoad_Click(object sender, EventArgs e)
+        //{
+        //    dataGridView1.DataSource = dl.GetRecords();
+        //}
+
+        //private void btnDelete_Click(object sender, EventArgs e)
+        //{
+        //    var x = dataGridView1.SelectedRows[0];
+        //    var z = x.Cells[0].Value;
+        //    dl.DeleteRecord(int.Parse((string)z));
+
+        //    dataGridView1.DataSource = dl.GetRecords();
+        //}
+
+        //private void btnUpdate_Click(object sender, EventArgs e)
+        //{
+        //    var x = dataGridView1.SelectedRows[0];
+        //    var z = x.Cells[0].Value;
+        //    dl.UpdateRecord(int.Parse((string)z), txtTestData.Text, DateTime.Now);
+
+        //    dataGridView1.DataSource = dl.GetRecords();
+        //}
     }
 }
