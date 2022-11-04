b850aa1cf99a75e80dc13820fb09a651d9199a9b
Shabs <85132269+doshabsa@users.noreply.github.com>
2022-11-04 14:41:20 -0600

    Added sample code from previous phidget demo

--- 270_GeoLocBox/270_GeoLocBox/Form1.cs
+++ 270_GeoLocBox/270_GeoLocBox/Form1.cs
@@ -8,6 +8,15 @@ namespace _270_GeoLocBox
         public Form1()
         {
             InitializeComponent();
+
+            //Demo code from button quiz thing
+            //button1 = new DigitalInput();
+            //button1.Channel = 0;
+            //button1.HubPort = 0;
+            //button1.IsHubPortDevice = true;
+            //button1.StateChange += Button1_StateChange;
+            //button1.StateChange += Button1_StateChange1;
+            //button1.Open(1000);
         }
     }
 }
